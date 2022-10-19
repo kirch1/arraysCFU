9986be8e0056b2434857c778babe9f17eac0c083
Jason Kirchman <kirchman.jason@icloud.com>
2022-10-18 23:04:56 -0500

    Add index understanding

--- arrays.js
+++ arrays.js
@@ -10,3 +10,7 @@ quantities.push(6);
 
 //The slice method below  will return a portion of the array starting from index 1 and ending at 3. 3 is not included
 needMore.slice(1,3);
+
+//An index is the position of the element in an array. We can use the index to acess values in the array.
+//Assuming the arrays index correspond to eachother, the strin interpolation below prints information about the product
+console.log(`The product ${fruits[1]} has a stock quantity of ${quantities[1]} and it is ${needMore[1]} that we need more.`)
