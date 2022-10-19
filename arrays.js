e1cd044e5288a56b7acef410448e7ceb9dd3880e
Jason Kirchman <kirchman.jason@icloud.com>
2022-10-18 22:49:00 -0500

    Call methods on three arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var fruits = ["apple","grape","banana","cherry"];
 var quantities = [2,7,1,9];
 var needMore = [true,false,true,false];
+
+//The pop method will remove and return the last element in the fruits array, which is "cherry";
+fruits.pop();
+
+//The push method will add the element 6 to the end of the quantites array.
+quantities.push(6);
+
+//The slice method below  will return a portion of the array starting from index 1 and ending at 3. 3 is not included
+needMore.slice(1,3);
